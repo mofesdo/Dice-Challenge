3e66f012ee82adb683a9fac4b0058c6cf7670fc1
Modesto Amador <mofesdo.dev@gmail.com>
2022-05-11 20:10:42 -0700

    dice challenge done

--- index.js
+++ index.js
@@ -5,3 +5,13 @@ console.log(randomNumber2);
 
 document.getElementById("img1").setAttribute("src", "images/dice"+randomNumber1+".png");
 document.getElementById("img2").setAttribute("src", "images/dice"+randomNumber2+".png");
+
+if(randomNumber1 > randomNumber2){
+  document.getElementById("winner").textContent = "🚩 Player 1 Wins!";
+}
+else if(randomNumber1 < randomNumber2){
+  document.getElementById("winner").textContent = "Player 2 Wins! 🚩";
+}
+else{
+    document.getElementById("winner").textContent = "Draw!";
+}
